df42008a1619ac041ffef436daf519b73b257680
Gabor Szabo <gabor.szabo86@hotmail.com>
2023-10-09 13:55:19 +0200

    First commit

---
+++ app2/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'app2';
+
+  constructor(private oauth: OAuthService) {}
+
+  logout() {
+    this.oauth.logOut();
+  }
+}
